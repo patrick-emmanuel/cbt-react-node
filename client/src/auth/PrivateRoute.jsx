d0360ec27036e8f17982aaaeb50736be7acc3faf
Patrick <inem.patrick@gmail.com>
2019-09-04 13:29:31 +0100

    Create private router

---
+++ client/src/auth/PrivateRoute.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import { getAuthToken } from '../utils/auth';
+
+const SectionRoute = ({
+  component: Component,
+  ...rest
+}) => {
+  const token = getAuthToken();
+  // Implement permissions here.
+  return (
+    <>
+      <Route
+        {...rest}
+        render={props =>
+          token ? (
+            <div>
+              <Component {...props} />
+            </div>
+          ) : (
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: props.location }
+              }}
+            />
+          )
+        }
+      />
+    </>
+  );
+};
+
+SectionRoute.displayName = 'Route';
+export default SectionRoute;
