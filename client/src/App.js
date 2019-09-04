a8f4d1c3cd968ab5e6c09a7a91872a36ac024e87
Patrick <inem.patrick@gmail.com>
2019-09-04 13:29:12 +0100

    Setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import AuthProvider from './auth/AuthProvider';
+import PrivateRoute from './auth/PrivateRoute';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './auth/Login';
+import SignUp from './auth/SignUp';
+import Home from './home';
 import './App.css';
 
 const App = () => {
-  return <div>Apollo</div>
+  return (
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <PrivateRoute exact path="/" component={Home} />
+        </Switch>
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
